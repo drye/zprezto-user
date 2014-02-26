aba9c9361b9488a9765bd9cc6323ec27de5541c3
Primož Verdnik <primoz.verdnik@gmail.com>
2014-02-26 16:07:30 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'primoz.verdnik@gmail.com'
 license          'MIT'
 description      'Provides a simple definition for installing zprezto (zsh) for a given user'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 depends 'git'
 depends 'zsh'
